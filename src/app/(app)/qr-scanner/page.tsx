e9b83bedafc0094369d92fe233989adb0b2321d1
Cedric <cao.sinaon@gmail.com>
2025-09-26 10:34:20 +0800

    admin qr scanner

--- src/app/(app)/qr-scanner/page.tsx
+++ src/app/(app)/qr-scanner/page.tsx
@@ -8,6 +8,10 @@ export default async function QRScannerPage() {
     redirect("/");
   }
 
+  if (session?.user.role != "admin") {
+    redirect("/profile");
+  }
+
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
